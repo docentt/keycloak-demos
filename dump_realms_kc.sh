645056c903ec08b0bf1c360746555f2095f6efdb
Tomasz Kuczyński <docentt@man.poznan.pl>
2024-10-09 22:59:48 +0200

    Poprawka w skrypcie do robienia zrzutów konfiguracji.

--- dump_realms_kc.sh
+++ dump_realms_kc.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-KC24=$(docker ps --filter "name=keycloak-demos")
+KC=$(docker ps --filter "name=keycloak-demos")
 currentTime=$(date "+%Y.%m.%d-%H.%M.%S")
 
-if [ -z ""${KC24##*keycloak-24*} ]; then
-	docker exec keycloak-24 rm -rf /opt/keycloak/DUMP
-	docker exec keycloak-24 mkdir /opt/keycloak/DUMP
-	docker exec keycloak-24 /bin/sh -c 'export JAVA_OPTS_APPEND="" && /opt/keycloak/bin/kc.sh export --dir /opt/keycloak/DUMP'
-	docker cp keycloak-24:/opt/keycloak/DUMP .
+if [ -z ""${KC##*keycloak-demos*} ]; then
+	docker exec keycloak-demos rm -rf /opt/keycloak/DUMP
+	docker exec keycloak-demos mkdir /opt/keycloak/DUMP
+	docker exec keycloak-demos /bin/sh -c 'export JAVA_OPTS_APPEND="" && /opt/keycloak/bin/kc.sh export --dir /opt/keycloak/DUMP'
+	docker cp keycloak-demos:/opt/keycloak/DUMP .
 	mv DUMP DUMPS/$currentTime
 else
 	echo "Not dumping, keycloak-demos is not running"
