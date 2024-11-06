f524655f88ffdb3baed9bddddd22ed329978db34
Tomasz Kuczyński <docentt@man.poznan.pl>
2024-11-06 22:38:37 +0100

    Poprawka w skrypcie do dump.

--- dump_realms_kc.sh
+++ dump_realms_kc.sh
@@ -8,7 +8,7 @@ if [ -z ""${KC##*keycloak-demos*} ]; then
 	docker rmi keycloak-demos-dump
   docker commit keycloak-demos keycloak-demos-dump
   mkdir DUMPS/$currentTime
-  docker run --name=keycloak-demos-dump -v ./DUMPS/$currentTime:/opt/keycloak/DUMP --entrypoint /opt/keycloak/bin/kc.sh keycloak-demos-dump export --dir /opt/keycloak/DUMP
+  docker run --name=keycloak-demos-dump -v $(pwd)/DUMPS/$currentTime:/opt/keycloak/DUMP --entrypoint /opt/keycloak/bin/kc.sh keycloak-demos-dump export --dir /opt/keycloak/DUMP
 	docker rm keycloak-demos-dump
 	docker rmi keycloak-demos-dump
 else
