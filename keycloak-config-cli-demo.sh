f0ae908b9c9c84d5a9e52f22f5c6106abaaf36c9
Tomasz Kuczyński <tomasz.kuczynski@man.poznan.pl>
2025-01-08 19:21:38 +0100

    Zmiana w skrypcie pod scenariusze demonstracyjne

--- keycloak-config-cli-demo.sh
+++ keycloak-config-cli-demo.sh
@@ -12,7 +12,7 @@ if is_container_running "keycloak-demos"; then
       -e KEYCLOAK_PASSWORD=admin \
       -e KEYCLOAK_SSLVERIFY=false \
       -e LOGGING_LEVEL_ROOT=info \
-      -e IMPORT_VARSUBSTITUTION_ENABLED=true \
+      -e IMPORT_VARSUBSTITUTION_ENABLED=false \
       -e IMPORT_MANAGED_GROUP=full \
       -e IMPORT_MANAGED_CLIENTSCOPE=no-delete \
       -e IMPORT_MANAGED_CLIENT=full \
